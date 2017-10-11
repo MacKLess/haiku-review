77213ef3dc347e850eb920d497dc66e31d52ceb6
L. Devin MacKrell <ldmackrell@gmail.com>
2017-10-11 12:54:12 -0700

    Update app to include all installs but there is broken functionality for gulp browserify.

--- js/haikureview.js
+++ js/haikureview.js
@@ -4,6 +4,11 @@ function Haiku(line1,line2,line3) {
   this.line3 = line3;
 }
 
+// Will want to look for newlines "/n"
+//if "/n" !== 2, return "this is not a haiku"
+
+// Might need to revise arguments/parameters --> the above one is supposed to be "constructorParameter"; wherease the prototype argument is supposed to be the "methodParameter." review page "https://www.learnhowtoprogram.com/javascript/introduction-to-javascript/object-oriented-javascript-with-node-modules"
+
 Haiku.prototype.haikuReview = function(line1,line2,line3) {
   let syllable = 0;
   let output = [];
@@ -12,12 +17,25 @@ Haiku.prototype.haikuReview = function(line1,line2,line3) {
     if (i != vowel) {
       output.push(i);
     } else {
-      if i+1 != vowel
+      if i-1 === vowel
+        output.push(i);
+      else if i+1 === vowel
         output.push(i);
         syllable +1;
-      else {
+      else if i+1 === "e"
         output.push(i);
+      else { i+1 != vowel
+        output.push(i);
+        syllable +1;
       }
     }
   }
+  if (syllable != 17) {
+    return "This is not a haiku."
+  }
+  else {
+    return output1; //This will require additional work
+  }
 }
+
+exports.haikuModule = Haiku;
